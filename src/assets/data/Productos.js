76f076e082ae5b1fac0a67b6c250612d64d06b05
Lucas <lucasexrojas@gmail.com>
2023-08-05 12:24:09 -0300

    ultima correcion rutas

--- src/assets/data/Productos.js
+++ src/assets/data/Productos.js
@@ -14,37 +14,37 @@ export const products = [
 	{
 		id: 4,
 		title: "Lima",
-		img: ".img/Productos/sabor-lima.jpg",
+		img: "./img/Productos/sabor-lima.jpg",
 		category: "Cabalgata-lima",
 	},
 	{
 		id: 6,
 		title: "Pomelo",
-		img: ".img/Productos/sabor-pomelo.jpg",
+		img: "./img/Productos/sabor-pomelo.jpg",
 		category: "Cabalgata-Pomelo",
 	},
 	{
 		id: 7,
 		title: "Soda",
-		img: ".img/Productos/soda.jpg",
+		img: "./img/Productos/soda.jpg",
 		category: "Cabalgata-Soda",
 	},
 	{
 		id: 10,
 		title: "Agua",
-		img: ".img/Productos/agua.jpg",
+		img: "./img/Productos/agua.jpg",
 		category: "Cabalgata-agua",
 	},
 	{
 		id: 11,
 		title: "Manzana",
-		img: ".img/Productos/manzana.jpg",
+		img: "./img/Productos/manzana.jpg",
 		category: "Cabalgata-manzana",
 	},
 	{
 		id: 13,
 		title: "Guarana",
-		img: ".img/Productos/guarana.jpg",
+		img: "./img/Productos/guarana.jpg",
 		category: "Cabalgata-guarana",
 	},
 ];
